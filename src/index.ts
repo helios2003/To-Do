ac4726919d06cb5bafa7768d061329c63ff1390b
Ankit Dash <ankitdash2019@gmail.com>
2023-08-23 02:13:46 +0530

    created the app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,98 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+function generateRandomId(length: number): string {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let randomId = '';
 
-import confetti from 'canvas-confetti';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomId += characters[randomIndex];
+  }
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+  return randomId;
+}
+
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.getElementById("new-task-form") as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
+
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (input?.value == "" || input?.value == null) return;
+
+  const newTask: Task = {
+    id: generateRandomId(5),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  tasks.push(newTask);
+
+  addListItem(newTask);
+  input.value = "";
+});
+
+function addListItem(task: Task) {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  });
+
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
+
+  const createdAtFormatted = task.createdAt.toLocaleString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
+
+  label.append(checkbox, `${task.title} - Created on: ${createdAtFormatted}`);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", () => {
+    deleteTask(task);
+    item.remove();
+  });
+
+  label.appendChild(deleteButton);
+
+  item.append(label);
+  list?.appendChild(item); 
+}
+
+function deleteTask(task: Task) {
+  const taskIndex = tasks.findIndex((t) => t.id === task.id);
+  if (taskIndex !== -1) {
+    tasks.splice(taskIndex, 1);
+    saveTasks();
+  }
+}
+
+// so that the user does not loose the tasks they made a list of
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS");
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+}
